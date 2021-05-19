8efc4ffe6bd5881884eece235b1a18cfee8acb51
Arnab Biswas <arnabbiswas@yahoo.com>
2021-05-18 23:27:17 -0400

    Bugfix - reversed directories while popping

--- Lecture-4/9-simplifyPath.py
+++ Lecture-4/9-simplifyPath.py
@@ -3,14 +3,14 @@ Simpler variation of decodeString
 """
 def simplifyPath(expr):
     stack=[]
-    for folders in expr.split('/'): #Had to take this approach instead to split by folders
-        if stack and folders == '..':
+    for folder in expr.split('/'): #Had to take this approach instead to split by folders
+        if stack and folder == '..':
             stack.pop()
-        elif folders not in ['.', '..', '']: #Remember to add empty string too
-            stack.append(folders)
+        elif folder not in ['.', '..', '']: #Remember to add empty string too
+            stack.append('/'+folder)
     out=''
     while stack:
-        out += '/'+stack.pop() #append in reverse
+        out = stack.pop() + out#append in reverse
     return out
 
-print(simplifyPath("/a/./b/../../c/"))
+print(simplifyPath("/a/./b/../c/"))
